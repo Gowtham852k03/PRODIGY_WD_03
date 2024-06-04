a5108f6fa2690fec3ae5ffb1fc01b571ccd38ab7
Your Name <your_email@example.com>
2024-06-04 11:36:49 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Tic_Tac_Toe from './Tic_Tac_Toe';
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tic_Tac_Toe />
+    </div>
+  );
+}
+
+export default App;
